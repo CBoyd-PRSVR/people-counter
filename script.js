923db18d857ef014c45ec14fa1df6928d8e63fd0
CBoyd-PRSVR <cboyd.prsvr@gmail.com>
2024-04-17 20:56:29 -0400

    round1

---
+++ script.js
@@ -0,0 +1,28 @@
+// document.getElementById("count").innerText = 5
+
+// let firstBatch = 5
+// console.log(firstBatch)
+
+// let secondBatch = 7
+// console.log(secondBatch)
+
+// let count = firstBatch + secondBatch
+// console.log(count)
+
+
+// let myAge = 23
+// let humanDogRatio = 7
+// let myDogAge = myAge * humanDogRatio
+// console.log(myDogAge)
+
+// let count = 5
+// count = count + 1
+// console.log(count)
+
+let bonusPoints = 50
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
