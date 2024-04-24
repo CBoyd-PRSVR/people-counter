91e7386e29c7692305665af27e26be3229001509
CBoyd-PRSVR <cboyd.prsvr@gmail.com>
2024-04-23 20:51:28 -0400

    js, my beloved, to thee i cling

---
+++ notes.js
@@ -0,0 +1,89 @@
+// document.getElementById("count").innerText = 5
+
+// let firstBatch = 5
+// console.log(firstBatch)
+
+// let secondBatch = 7
+// console.log(secondBatch)
+
+// let count = firstBatch + secondBatch
+// console.log(count)
+
+
+// let myAge = 23
+// let humanDogRatio = 7
+// let myDogAge = myAge * humanDogRatio
+// console.log(myDogAge)
+
+// let count = 5
+// count = count + 1
+// console.log(count)
+
+//let bonusPoints = 50
+//bonusPoints = bonusPoints + 50
+//console.log(bonusPoints)
+//bonusPoints = bonusPoints - 75
+//console.log(bonusPoints)
+//bonusPoints = bonusPoints + 45
+//console.log(bonusPoints)
+
+//function countdown() {
+   // console.log(5)
+   // console.log(4)
+   // console.log(3)
+   // console.log(2)
+   // console.log(1)
+//}
+
+//countdown()
+
+//function hamburger() {
+  //  console.log(42)
+//}
+//hamburger()
+
+//let lapsCompleted =0
+
+////function  incrementLap( ) {
+//lapsCompleted = lapsCompleted + 1
+//}
+
+//incrementLap()
+//incrementLap()
+//incrementLap()
+
+//console.log(lapsCompleted)
+
+//function increment() {
+//    console.log(theButtonWasClicked)
+//}
+//let username = "chris"
+
+//console.log(username)
+
+
+//let message = "you have 3 new notifications"
+//console.log(message)
+
+// let myName = "Chris"
+// console.log(myName)
+// let greeting = "Hi, my name is"
+// console.log(greeting)
+// let myGreeting = greeting + ", " + myName
+// console.log(myGreeting)
+//let myName = " time to die."
+//console.log(myName)
+//let greeting = "Rides over Mutie,"
+//console.log(greeting)
+
+//et welcomeEl = document.getElementById("welcome-el")
+//welcomeEl.innerText = greeting + myName
+// console.log(welcomeEl)
+// welcomeEl.innerText = welcomeEl.innerText + " jk, luv you"
+// or
+// welcomeEl.innerText += " jk, luv you"
+
+// let firstName = "Chris"
+// let lastName = "Boyd"
+// let fullName = firstName + " " + lastName
+// console.log(fullName)
--- script.js
+++ script.js
@@ -1,102 +1,19 @@
-// document.getElementById("count").innerText = 5
-
-// let firstBatch = 5
-// console.log(firstBatch)
-
-// let secondBatch = 7
-// console.log(secondBatch)
-
-// let count = firstBatch + secondBatch
-// console.log(count)
-
-
-// let myAge = 23
-// let humanDogRatio = 7
-// let myDogAge = myAge * humanDogRatio
-// console.log(myDogAge)
-
-// let count = 5
-// count = count + 1
-// console.log(count)
-
-//let bonusPoints = 50
-//bonusPoints = bonusPoints + 50
-//console.log(bonusPoints)
-//bonusPoints = bonusPoints - 75
-//console.log(bonusPoints)
-//bonusPoints = bonusPoints + 45
-//console.log(bonusPoints)
-
-//function countdown() {
-   // console.log(5)
-   // console.log(4)
-   // console.log(3)
-   // console.log(2)
-   // console.log(1)
-//}
-
-//countdown()
-
-//function hamburger() {
-  //  console.log(42)
-//}
-//hamburger()
-
-//let lapsCompleted =0
-
-////function  incrementLap( ) {
-//lapsCompleted = lapsCompleted + 1
-//}
-
-//incrementLap()
-//incrementLap()
-//incrementLap()
-
-//console.log(lapsCompleted)
-
-//function increment() {
-//    console.log(theButtonWasClicked)
-//}
-//let username = "chris"
-
-//console.log(username)
-
-
-//let message = "you have 3 new notifications"
-//console.log(message)
-
-// let myName = "Chris"
-// console.log(myName)
-// let greeting = "Hi, my name is"
-// console.log(greeting)
-// let myGreeting = greeting + ", " + myName
-// console.log(myGreeting)
-let myName = " time to die."
-console.log(myName)
-let greeting = "Rides over Mutie,"
-console.log(greeting)
-
-let welcomeEl = document.getElementById("welcome-el")
-welcomeEl.innerText = greeting + myName
-console.log(welcomeEl)
-
-
-
-
-
-
 let countEl = document.getElementById("count-el")
 console.log(countEl)
-
+let saveEl = document.getElementById("save-el")
+console.log(saveEl)
 let count = 0 
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+    count += 1
+    countEl.textContent = count
     console.log(count)
-
 }
 
 function save() {
+let countText = count + " - "
+saveEl.textContent += countText
 console.log(count)
+count = 0
+countEl.textContent = count
 }
